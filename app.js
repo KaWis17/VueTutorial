45832591dd87193b443f52c1b50d9a89fafb12ad
kawis <krzymado@gmail.com>
2023-12-23 01:51:46 +0100

    Working with data

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-// in mount() we assign to which element 
-Vue.createApp({}).mount('#app')
+// in mount() we insert an application into the document
+// we say where to render the data
+Vue.createApp({
+    data() {
+        return {
+            firstName: "Krzysztof",
+            lastName: "Wisniewski"
+        }
+    }
+}).mount('#app')
